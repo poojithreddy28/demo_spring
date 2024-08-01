5c479b32cc12670a16636596f3f2471bfcbf067a
PoojithReddy <poojithannachedu.work@gmail.com>
2024-08-01 17:34:09 -0500

    initial commit

---
+++ proejcts/first_project/src/test/java/com/spring_boot/first_project/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot.first_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
